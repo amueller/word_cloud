79186a56eac54b3b87c0dbb244c469c80c41c06d
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-11-06 20:37:39 +0000

    enh more comments, redraw in color, add margin argument

--- wordcloud.py
+++ wordcloud.py
@@ -7,24 +7,30 @@ from sklearn.feature_extraction.text import CountVectorizer
 from query_integral_image import query_integral_image
 
 
-def make_wordcloud(words, counts, width=400, height=200):
+#@profile
+def make_wordcloud(words, counts, width=400, height=200, margin=20):
     # sort words by counts
     inds = np.argsort(counts)[::-1]
     counts = counts[inds]
     words = words[inds]
     # create image
-    img = Image.new("L", (width, height))
-    draw = ImageDraw.Draw(img)
+    img_grey = Image.new("L", (width, height))
+    draw = ImageDraw.Draw(img_grey)
     #i = 0
     integral = np.zeros((height, width), dtype=np.uint)
-    img_array = np.asarray(img)
+    img_array = np.asarray(img_grey)
+    font_sizes, positions, orientations = [], [], []
+    # intitiallize fontsize "large enough"
+    font_size = 1000
+    # start drawing grey image
     for word, count in zip(words, counts):
         font_path = "/usr/share/fonts/truetype/droid/DroidSansMono.ttf"
         #img_array = img_array.sum(axis=2)
         # set font size
-        font_size = int(np.log(count)) * 20
+        font_size = min(font_size, int(np.log(count)) * 20)
         runs = 0
         while True:
+            # try to find a position
             font = ImageFont.truetype(font_path, font_size)
             # transpose font optionally
             orientation = random.choice([None, Image.ROTATE_90])
@@ -32,22 +38,26 @@ def make_wordcloud(words, counts, width=400, height=200):
                                                        orientation=orientation)
             draw.setfont(transposed_font)
             # get size of resulting text
-            size = draw.textsize(word)
+            box_size = draw.textsize(word)
             # find possible places using integral image:
-            result = query_integral_image(integral, size[1], size[0])
+            result = query_integral_image(integral, box_size[1] + margin, box_size[0] + margin)
             if result is not None:
                 break
             # if we didn't find a place, make font smaller
             font_size -= 1
             runs += 1
+        # actually draw the text
         x, y = result
-        draw.text((result[1], result[0]), word, fill="white")
+        draw.text((y, x), word, fill="white")
+        positions.append((x, y))
+        orientations.append(orientation)
+        font_sizes.append(font_size)
         # recompute integral image
-        img_array = np.asarray(img)
+        img_array = np.asarray(img_grey)
         # recompute bottom right
         partial_integral = np.cumsum(np.cumsum(img_array[x:, y:], axis=1),
                                      axis=0)
-        # paste into old image
+        # paste recomputed part into old image
         # if x or y is zero it is a bit annoying
         if x > 0:
             if y > 0:
@@ -60,6 +70,18 @@ def make_wordcloud(words, counts, width=400, height=200):
 
         integral[x:, y:] = partial_integral
 
+    # redraw in color
+    img = Image.new("RGB", (width, height))
+    draw = ImageDraw.Draw(img)
+    everything = zip(words, font_sizes, positions, orientations)
+    for word, font_size, position, orientation in everything:
+        font = ImageFont.truetype(font_path, font_size)
+        # transpose font optionally
+        transposed_font = ImageFont.TransposedFont(font,
+                                                   orientation=orientation)
+        draw.setfont(transposed_font)
+        draw.text((position[1], position[0]), word,
+                  fill="hsl(%d" % random.randint(0, 255) + ", 50%, 50%)")
     img.show()
 
 if __name__ == "__main__":
