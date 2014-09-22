c1014d10eeee375ed786755cd16fc5c802bee274
Andreas Mueller <amueller@ais.uni-bonn.de>
2014-09-22 19:47:29 +0200

    Add example using mask

---
+++ examples/masked.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+
+from os import path
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+
+from wordcloud import WordCloud, STOPWORDS
+
+d = path.dirname(__file__)
+
+# Read the whole text.
+text = open(path.join(d, 'alice.txt')).read()
+
+# read the mask image
+# taken from
+# http://www.stencilry.org/stencils/movies/alice%20in%20wonderland/255fk.jpg
+alice_mask = imread(path.join(d, "alice_mask.png"))
+plt.imshow(alice_mask)
+
+
+wc = WordCloud(background_color="white", max_words=2000, mask=alice_mask,
+               stopwords=STOPWORDS.add("said"))
+wc.generate(text)
+plt.figure()
+plt.imshow(wc)
+plt.show()
