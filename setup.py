c990bc9eb6fe2bc615f435bc84b65e206c227ea7
Andreas Mueller <amueller@ais.uni-bonn.de>
2015-06-19 16:28:34 -0400

    add C file for distribution, bump minor version

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
-from Cython.Build import cythonize
+from distutils.extension import Extension
 
 setup(
     name='wordcloud',
-    version='1.1.0',
+    version='1.1.1',
     url='https://github.com/amueller/word_cloud',
     description='A little word cloud generator',
     license='MIT',
-    ext_modules=cythonize('wordcloud/query_integral_image.pyx'),
+    ext_modules=[Extension("wordcloud/query_integral_image",
+                           ["wordcloud/query_integral_image.c"])],
     packages=['wordcloud'],
     package_data={'wordcloud': ['stopwords']}
 )
