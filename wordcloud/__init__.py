b319cf4f2484a6540f23c8ee5aab5bf9c7f6fbef
Andreas Mueller <t3kcit@gmail.com>
2017-03-21 16:46:18 -0400

    release yet again, make __version__ a string.

--- wordcloud/__init__.py
+++ wordcloud/__init__.py
@@ -1,5 +1,7 @@
-from .wordcloud import WordCloud, STOPWORDS, random_color_func, get_single_color_func
+from .wordcloud import (WordCloud, STOPWORDS, random_color_func,
+                        get_single_color_func)
 from .color_from_image import ImageColorGenerator
 
-__all__ = ['WordCloud', 'STOPWORDS', 'random_color_func', 'get_single_color_func', 'ImageColorGenerator']
-__version__ = 1.3
+__all__ = ['WordCloud', 'STOPWORDS', 'random_color_func',
+           'get_single_color_func', 'ImageColorGenerator']
+__version__ = "1.3.1"
