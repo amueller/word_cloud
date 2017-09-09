80a2c2bf884b5163f4d4afc1a8bcbdae29da1f11
Raphael Boidol <r.boidol+github@gmail.com>
2017-09-09 15:52:23 +0200

    Only maintain version info once

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import re
+import io
 from setuptools import setup
 from setuptools.extension import Extension
+
+__version__ = re.search(
+    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
+    io.open('wordcloud/__init__.py', encoding='utf_8').read()
+    ).group(1)
 
 setup(
     author="Andreas Mueller",
     author_email="t3kcit+wordcloud@gmail.com",
     name='wordcloud',
-    version='1.3.1',
+    version=__version__,
     url='https://github.com/amueller/word_cloud',
     description='A little word cloud generator',
     license='MIT',
