8338150e75319762ed8912075c09008ed05736ee
Raphael Boidol <r.boidol+github@gmail.com>
2017-09-09 20:14:00 +0200

    Update conda link to avoid redirects

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -20,7 +20,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
 
     # Use the miniconda installer for faster download / install of conda
     # itself
-    wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh \
+    wget https://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh \
         -O miniconda.sh
     chmod +x miniconda.sh && ./miniconda.sh -b
     export PATH=/home/travis/miniconda/bin:$PATH
