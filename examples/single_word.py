4cd7af3850d76fc265a735b195571f7298cf733d
Andreas Müller <t3kcit@gmail.com>
2018-06-14 14:00:12 -0700

    add example with single word

---
+++ examples/single_word.py
@@ -0,0 +1,16 @@
+# make a word cloud with a single word that's repeated.
+import numpy as np
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+text = "square"
+
+x, y = np.ogrid[:300, :300]
+
+mask = (x - 150) ** 2 + (y - 150) ** 2 > 130 ** 2
+mask = 255 * mask.astype(int)
+plt.imshow(mask)
+plt.figure()
+
+wc = WordCloud(repeat=True, mask=mask).generate(text)
+plt.imshow(wc)
+plt.show()
