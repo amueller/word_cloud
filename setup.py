5357b282b17efabf5feb276ea65b078e6c3e0091
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-07-17 03:05:48 -0400

    setup: Set "long_description" property based on README.md

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ __version__ = re.search(
     io.open('wordcloud/__init__.py', encoding='utf_8').read()
     ).group(1)
 
+
+with io.open('README.md', encoding='utf_8') as fp:
+    readme = fp.read()
+
 setup(
     author="Andreas Mueller",
     author_email="t3kcit+wordcloud@gmail.com",
@@ -15,6 +19,8 @@ setup(
     version=__version__,
     url='https://github.com/amueller/word_cloud',
     description='A little word cloud generator',
+    long_description=readme,
+    long_description_content_type='text/markdown; charset=UTF-8',
     license='MIT',
     install_requires=['numpy>=1.6.1', 'pillow'],
     test_requires=['matplotlib'],
