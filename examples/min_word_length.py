849b4a545125d4ed375b29719db8bd2fddf002a5
The-RCCG <r.greif@outlook.com>
2019-01-27 14:56:36 +0100

    exampels for min_word_length and number_frequencies

---
+++ examples/min_word_length.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+Using min words analyse A New Hope
+===================
+"""
+
+from os import path, getcwd
+import matplotlib.pyplot as plt
+
+from wordcloud import WordCloud
+
+
+# get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+d = path.dirname(__file__) if "__file__" in locals() else getcwd()
+
+# movie script of "a new hope"
+# http://www.imsdb.com/scripts/Star-Wars-A-New-Hope.html
+# May the lawyers deem this fair use.
+text = open(path.join(d, 'a_new_hope.txt')).read()
+
+wc = WordCloud(max_words=1000, margin=10,
+               random_state=1, min_word_length=5).generate(text)
+# store default colored image
+default_colors = wc.to_array()
+#fig, ax = plt.subplots(1,1)
+plt.imshow(wc)
+wc.to_file("min_word_length_a_new_hope.png")
+plt.axis("off")
+plt.show()
