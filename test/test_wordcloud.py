4a24674f513df86ec19fd941dc512c48d01115b7
The-RCCG <r.greif@outlook.com>
2019-01-31 07:35:21 +0100

    formatting adjustments to test_wordcloud.py

--- test/test_wordcloud.py
+++ test/test_wordcloud.py
@@ -32,11 +32,11 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 
-3 . 1 4 1 5 9   2 6 5 3 5   8 9 7 9 3   2 3 8 4 6   2 6 4 3 3   
-    8 3 2 7 9   5 0 2 8 8   4 1 9 7 1   6 9 3 9 9   3 7 5 1 0   
-    5 8 2 0 9   7 4 9 4 4   5 9 2 3 0   7 8 1 6 4   0 6 2 8 6   
-    2 0 8 9 9   8 6 2 8 0   3 4 8 2 5   3 4 2 1 1   7 0 6 7 9   
-    8 2 1 4 8   0 8 6 5 1   3 2 8 2 3   0 6 6 4 7   0 9 3 8 4   
+3 . 1 4 1 5 9   2 6 5 3 5   8 9 7 9 3   2 3 8 4 6   2 6 4 3 3
+    8 3 2 7 9   5 0 2 8 8   4 1 9 7 1   6 9 3 9 9   3 7 5 1 0
+    5 8 2 0 9   7 4 9 4 4   5 9 2 3 0   7 8 1 6 4   0 6 2 8 6
+    2 0 8 9 9   8 6 2 8 0   3 4 8 2 5   3 4 2 1 1   7 0 6 7 9
+    8 2 1 4 8   0 8 6 5 1   3 2 8 2 3   0 6 6 4 7   0 9 3 8 4
     4 6 0 9 5   5 0 5 8 2   2 3 1 7 2   5 3 5 9 4   0 8 1 2 8
 """
 
@@ -387,5 +387,3 @@ def test_repeat():
     frequencies = [w[0][1] for w in wc.layout_]
     # check that frequencies are sorted
     assert np.all(np.diff(frequencies) <= 0)
-
-
