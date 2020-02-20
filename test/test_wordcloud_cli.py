e3f950ec843cb3c46555e37fa827926db66ae080
Dmitry Logvinenko <5diezfun@gmail.com>
2020-02-20 11:23:23 +0300

    Fix an --include_number argument test

--- test/test_wordcloud_cli.py
+++ test/test_wordcloud_cli.py
@@ -25,7 +25,8 @@ ARGUMENT_SPEC_TYPED = [
     ArgOption(cli_name='relative_scaling', init_name='relative_scaling', pass_value=1, fail_value='c'),
 ]
 ARGUMENT_SPEC_UNARY = [
-    ArgOption(cli_name='no_collocations', init_name='collocations', pass_value=True, fail_value=1)
+    ArgOption(cli_name='no_collocations', init_name='collocations', pass_value=True, fail_value=1),
+    ArgOption(cli_name='include_numbers', init_name='include_numbers', pass_value=True, fail_value=2),
 ]
 ARGUMENT_SPEC_REMAINING = [
     ArgOption(cli_name='stopwords', init_name='stopwords', pass_value=PassFile(), fail_value=None),
@@ -36,7 +37,6 @@ ARGUMENT_SPEC_REMAINING = [
     ArgOption(cli_name='background', init_name='background_color', pass_value='grey', fail_value=None),
     ArgOption(cli_name='contour_color', init_name='contour_color', pass_value='grey', fail_value=None),
     ArgOption(cli_name='contour_width', init_name='contour_width', pass_value=0.5, fail_value='blue'),
-    ArgOption(cli_name='include_numbers', init_name='include_numbers', pass_value=True, fail_value=None),
     ArgOption(cli_name='min_word_length', init_name='min_word_length', pass_value=5, fail_value='blue'),
 ]
 ARGUMENT_CLI_NAMES_UNARY = [arg_opt.cli_name for arg_opt in ARGUMENT_SPEC_UNARY]
