6cbaa2a213ca51b419a770093cc8d4b12d80b940
Thomas J. Fan <thomasjpfan@gmail.com>
2023-12-05 08:48:02 -0500

    CI Use pyproject_toml

--- wordcloud/__init__.py
+++ wordcloud/__init__.py
@@ -6,6 +6,5 @@ __all__ = ['WordCloud', 'STOPWORDS', 'random_color_func',
            'get_single_color_func', 'ImageColorGenerator',
            '__version__']
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
