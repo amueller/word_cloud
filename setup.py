2ee1e21a3c4fc1442eabfa448a6fa15e0f7e4469
Titanium <95677829+T1t4m1un@users.noreply.github.com>
2023-12-05 21:54:27 +0800

    Fix: #730 and #733 - Unable to build/install under Python 3.12 (#738)
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import io
 from setuptools import setup
-from setuptools.extension import Extension
 import versioneer
+
+from Cython.Build import cythonize
 
 with io.open('README.md', encoding='utf_8') as fp:
     readme = fp.read()
@@ -18,8 +19,7 @@ setup(
     long_description_content_type='text/markdown; charset=UTF-8',
     license='MIT',
     install_requires=['numpy>=1.6.1', 'pillow', 'matplotlib'],
-    ext_modules=[Extension("wordcloud.query_integral_image",
-                           ["wordcloud/query_integral_image.c"])],
+    ext_modules=cythonize("wordcloud/query_integral_image.pyx"),
     entry_points={'console_scripts': ['wordcloud_cli=wordcloud.__main__:main']},
     packages=['wordcloud'],
     package_data={'wordcloud': ['stopwords', 'DroidSansMono.ttf']}
